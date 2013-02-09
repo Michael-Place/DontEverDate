307886d3e87a851e0d680259355ed726b916d095
Zhe Jia <zjia@solstice-consulting.com>
2013-02-08 19:12:56 -0600

    added findDistanceFrom  findMidPointFrom

--- DontEverDate/Auxiliary.m
+++ DontEverDate/Auxiliary.m
@@ -28,5 +28,50 @@
     if (vector.y<0) alfa=alfa+180;
     return alfa;
 }
++(NSNumber *) findDistanceFrom:(CGPoint) point1 to:(CGPoint) point2{
+    float xdist;
+    float ydist;
+    float dist;
+    xdist = fabs(point1.x - point2.x);
+    ydist = fabs(point1.y - point2.y);
+    dist = sqrtf((xdist*xdist)+(ydist*ydist));
+    
+    return [NSNumber numberWithFloat:dist];
+    
+}
+
++(CGPoint) findMidPointFrom:(CGPoint) point1 to:(CGPoint) point2{
+    CGFloat xdist;
+    CGFloat ydist;
+    CGPoint midPoint;
+    xdist = fabs(point1.x - point2.x);
+    ydist = fabs(point1.y - point2.y);
+    
+    if (point1.x < point2.x) {
+        if (point1.y < point2.y) {
+            xdist = point1.x + (CGFloat)(xdist/2);
+            ydist = point1.y + (CGFloat)(ydist/2);
+            midPoint = CGPointMake(xdist,ydist);
+        }else {
+            xdist = point1.x + (CGFloat)(xdist/2);
+            ydist = point1.y - (CGFloat)(ydist/2);
+            midPoint = CGPointMake(xdist,ydist);
+        }
+    }else {
+        if (point1.y < point2.y) {
+            xdist = point1.x - (CGFloat)(xdist/2);
+            ydist = point1.y + (CGFloat)(ydist/2);
+            midPoint = CGPointMake(xdist,ydist);
+        }else {
+            xdist = point1.x - (CGFloat)(xdist/2);
+            ydist = point1.y - (CGFloat)(ydist/2);
+            midPoint = CGPointMake(xdist,ydist);
+        }
+        
+    }
+    
+    return midPoint;
+}
+
 
 @end
